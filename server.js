fb02fd586b75353010e108e900d09f9badd573dd
rclank <53981655+rclank@users.noreply.github.com>
2022-03-13 00:10:36 -0500

    routes started

--- server.js
+++ server.js
@@ -1,8 +1,20 @@
 const express = require('express');
 const db = require('./db/connection');
+const apiRoutes = require('./routes/apiRoutes');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
+
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+// use apiRoutes
+app.use('/api', apiRoutes);
+
+// default response for any other request (Not Found)
+app.use((req, res) => {
+    res.status(404).end();
+});
 
 db.connect(err => {
     if (err) throw err;
